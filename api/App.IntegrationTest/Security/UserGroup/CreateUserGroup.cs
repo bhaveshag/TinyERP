a86b59bd76225d259623ed7d97cc813cc0c4467a
marcelo-maciel <marcelo.mohr.maciel@gmail.com>
2016-12-18 13:01:52 -0200

    Adjusts for the 1st review of @tuanph

--- api/App.IntegrationTest/Security/UserGroup/CreateUserGroup.cs
+++ api/App.IntegrationTest/Security/UserGroup/CreateUserGroup.cs
@@ -18,13 +18,6 @@
         [TestMethod()]
         public void Security_UserGroup_CreateUserGroup_ShouldBeSuccess_WithValidRequest()
         {
-            //CreateUserGroupRequest request = new CreateUserGroupRequest($"Name {Guid.NewGuid()}", "desc");
-            //IResponseData<CreateUserGroupResponse> response = this.Connector.Post<CreateUserGroupRequest, CreateUserGroupResponse>(this.BaseUrl, request);
-            ///* Assert.IsTrue(response.Status == HttpStatusCode.OK); */
-            //Assert.IsTrue(response.Errors.Count == 0);
-            //Assert.IsTrue(response.Data != null);
-            //Assert.IsTrue(response.Data.Id != null && response.Data.Id != Guid.Empty);
-
             var permissions = new List<string>();
             CreateUserGroupRequest request = new CreateUserGroupRequest($"Name {Guid.NewGuid()}", "desc", permissions);
             IResponseData<CreateUserGroupResponse> response = this.Connector.Post<CreateUserGroupRequest, CreateUserGroupResponse>(this.BaseUrl, request);
@@ -35,10 +28,9 @@
         }
 
         [TestMethod()]
-        public void Security_UserGroup_CreateUserGroupn_ShouldThroException_WithInValidRequest()
+        public void Security_UserGroup_CreateUserGroup_ShouldThroException_WithEmptyName()
         {
             var permissions = new List<string>();
-            var key = Guid.NewGuid().ToString();
             CreateUserGroupRequest request = new CreateUserGroupRequest(string.Empty, string.Empty, permissions);
             IResponseData<CreateUserGroupResponse> response = this.Connector.Post<CreateUserGroupRequest, CreateUserGroupResponse>(this.BaseUrl, request);
             Assert.IsTrue(response.Errors.Count > 0);
@@ -46,7 +38,7 @@
         }
 
         [TestMethod()]
-        public void Security_UserGroup_CreateUserGroup_ShouldThroException_WithDuplicatedNameAndKey()
+        public void Security_UserGroup_CreateUserGroup_ShouldThroException_WithDuplicatedName()
         {
             var permissions = new List<string>();
             CreateUserGroupRequest request = new CreateUserGroupRequest($"Name {Guid.NewGuid()}", "desc", permissions);
